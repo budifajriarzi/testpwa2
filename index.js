6cda445c5e424cac02b013b441d0e57a1e887816
Paji <paji_starone@yahoo.co.id>
2018-07-12 00:51:01 +0700

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-function btnNOT_onClick(bgcolorcode){
+function btnDetails_onClick(bgcolorcode, title){
     $('#divDetails').css('visibility', 'visible');
     $('#divDetails').css('background-color', bgcolorcode)
+    $('#detailsTitle').html(title);
+    $('#form' + title).css('display', 'block');
     // alert('active');
 }
 
 function btnBack_onClick(){
     $('#divDetails').css('visibility', 'hidden');
+
+    var title = $('#detailsTitle').html();
+    $('#form' + title).css('display', 'none');
+}
+
+function btnSwitch(obj, n){
+    n = (n+1)%2;
+
+    if(n == 1){
+        
+        $(obj).css('background-color', '#FF0000');
+    } else if(n == 0){
+        $(obj).css('background-color', '#00FF00');
+    }
+
+    return n;
 }
