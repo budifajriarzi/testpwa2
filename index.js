c523da8ed263d88d8f12e2097a0f75bfffc828a7
Paji <paji_starone@yahoo.co.id>
2018-07-12 00:04:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function btnNOT_onClick(bgcolorcode){
+    $('#divDetails').css('visibility', 'visible');
+    $('#divDetails').css('background-color', bgcolorcode)
+    // alert('active');
+}
+
+function btnBack_onClick(){
+    $('#divDetails').css('visibility', 'hidden');
+}
